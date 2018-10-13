3a1ad14c31a6a0272b627500e9490421f4d64f8f
dianalimnos <dianalim1117@gmail.com>
2018-10-13 13:46:22 -0700

    implementing table view

---
+++ sustainabilityProject/Controllers/ListTasksTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ListTasksTableViewController.swift
+//  sustainabilityProject
+//
+//  Created by Diana Lim on 10/13/18.
+//  Copyright © 2018 Diana Lim. All rights reserved.
+//
+
+import UIKit
+
+class ListNotesTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 2
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listTasksTableViewCell", for: indexPath)
+        cell.textLabel?.text = "Cell Row: \(indexPath.row) Section: \(indexPath.section)"
+        
+        return cell
+    }
+    
+}
+
