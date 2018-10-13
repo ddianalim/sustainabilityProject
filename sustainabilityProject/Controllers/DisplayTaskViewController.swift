f21f251219b026c1d04854dbcc0b0b02bf07ab04
dianalimnos <dianalim1117@gmail.com>
2018-10-13 14:05:56 -0700

    added constraints

---
+++ sustainabilityProject/Controllers/DisplayTaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DisplayTaskViewController.swift
+//  sustainabilityProject
+//
+//  Created by Diana Lim on 10/13/18.
+//  Copyright © 2018 Diana Lim. All rights reserved.
+//
+
+import UIKit
+
+class DisplayNoteViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
---
+++ sustainabilityProject/Views/ListTasksTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ListTasksTableViewCell.swift
+//  sustainabilityProject
+//
+//  Created by Diana Lim on 10/13/18.
+//  Copyright © 2018 Diana Lim. All rights reserved.
+//
+
+import UIKit
+
+class ListNotesTableViewCell: UITableViewCell {
+    
+    
+}
+
