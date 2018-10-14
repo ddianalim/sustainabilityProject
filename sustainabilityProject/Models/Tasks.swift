30e29cd2c079844af41ab3a18b7f27aee4065eef
dianalimnos <dianalim1117@gmail.com>
2018-10-13 22:28:12 -0700

    cleaned up code

--- sustainabilityProject/Models/Tasks.swift
+++ sustainabilityProject/Models/Tasks.swift
@@ -20,9 +20,3 @@ class Task {
     }
 }
 
-/*
- var tasks1 = ["Reusable Bottle","Reusable Bag","Reusable Drink Container", "Packed Lunch", "Reusable Leftover Container", "Reusable straw/No straw", "Reusable Utensils", "Paperless Receipts"]
- 
- var points = ["5", "10", "15", "20", "_", "5", "5", "5"]
- 
- */
