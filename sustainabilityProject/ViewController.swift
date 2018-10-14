58181ab5d508e4288daf36f63d1572e16b53f055
dianalimnos <dianalim1117@gmail.com>
2018-10-14 12:55:56 -0700

    updating launch screen

--- sustainabilityProject/ViewController.swift
+++ sustainabilityProject/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    let texts = [
+        "What are you doing to help the earth?",
+        "Zero Waste for a better Earth!",
+        "Refuse, Reduce, Reuse, Recycle, Rot",
+        "Be remembered for your actions, not your trash",
+        "Keep making a difference!",
+        "Spread awareness about the zero waste lifestyle!",
+        "Welcome to nulla",
+        "Saving the earth one download at a time",
+        "Did you know that nulla means zero in Latin?",
+        ]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
